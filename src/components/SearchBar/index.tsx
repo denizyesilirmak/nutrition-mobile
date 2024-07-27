dd253490709d5b3c7f0627a9e978d313921d4c5b
yesilirmak <deniz.yesilirmak@sisal.com>
2024-07-27 14:42:16 +0300

    refactor: Update SearchBar component to use tailwind CSS classes for styling
    The SearchBar component has been updated to use tailwind CSS classes for styling instead of inline styles. This improves code maintainability and makes it easier to customize the component's appearance. The changes include:
    - Replaced inline styles with tailwind CSS classes for the container and input elements
    - Removed unused styles and properties
    - Updated the class names to match the tailwind CSS conventions
    
    These changes enhance the SearchBar component by leveraging the power of tailwind CSS and promoting a more consistent and scalable styling approach.

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -2,50 +2,27 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { router } from "expo-router";
 import { Pressable, TextInput, View } from "react-native";
 
-const SearchBar = () => {
+type SearchBarProps = {
+  onChangeText: (text: string) => void;
+};
+
+const SearchBar = ({ onChangeText }: SearchBarProps) => {
   return (
-    <View
-      style={{
-        width: "100%",
-        height: 60,
-        backgroundColor: "white",
-        justifyContent: "center",
-        padding: 8,
-      }}
-    >
-      <View
-        style={{
-          width: "100%",
-          height: "100%",
-          backgroundColor: "#ffffff",
-          borderRadius: 8,
-          borderColor: "#cccccc",
-          borderWidth: 1,
-          flexDirection: "row",
-        }}
-      >
+    <View className="h-20 w-full flex-row items-center justify-center bg-white p-2 dark:bg-black">
+      <View className="flex-row items-center justify-center rounded-2xl border border-gray-600 bg-white p-1 dark:bg-black">
         <TextInput
           placeholder="Search for foods"
           placeholderTextColor={"#666666"}
-          style={{
-            flex: 1,
-            paddingLeft: 12,
-          }}
+          onChangeText={onChangeText}
+          className="flex-1 pl-3 dark:text-white"
         />
         <Pressable
           onPress={() => {
             router.push("scanfood");
           }}
-          style={{
-            width: 44,
-            height: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-            borderLeftWidth: 1,
-            borderLeftColor: "#cccccc",
-          }}
+          className="rounded-r-lg bg-white p-2 dark:bg-black"
         >
-          <Ionicons name="scan-circle-outline" size={32} color="#666666" />
+          <Ionicons name="scan-circle-outline" size={30} color="#666666" />
         </Pressable>
       </View>
     </View>
