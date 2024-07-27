40caebf4a938cb8848cf3943c0fc85db5355ff9d
yesilirmak <deniz.yesilirmak@sisal.com>
2024-07-27 14:43:02 +0300

    Update Colors.ts to include LightTheme and DarkTheme
    The Colors.ts file has been updated to include LightTheme and DarkTheme objects. These objects define the color palettes for light and dark themes respectively. This change enhances the flexibility and customization options for the application's UI by providing predefined color schemes for different themes.

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -34,4 +34,28 @@ const COLORS = {
   status: STATUS_COLORS,
 };
 
+export const LightTheme = {
+  dark: false,
+  colors: {
+    primary: "#84cc16",
+    background: "rgb(255, 255, 255)",
+    card: "rgb(255, 255, 255)",
+    text: "rgb(28, 28, 30)",
+    border: "rgb(216, 216, 216)",
+    notification: "rgb(255, 59, 48)",
+  },
+};
+
+export const DarkTheme = {
+  dark: true,
+  colors: {
+    primary: "#22c55e",
+    background: "rgb(1, 1, 1)",
+    card: "rgb(18, 18, 18)",
+    text: "rgb(229, 229, 231)",
+    border: "rgb(39, 39, 41)",
+    notification: "rgb(255, 69, 58)",
+  },
+};
+
 export default COLORS;
