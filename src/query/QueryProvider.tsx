ac0de35884705508a79bf555fb48ce79ccd57597
yesilirmak <deniz.yesilirmak@sisal.com>
2024-07-27 14:43:14 +0300

    Add QueryProvider component for managing query client and app state
    The new QueryProvider component is added to manage the query client and app state. It wraps the application with the QueryClientProvider from the @tanstack/react-query package and sets up an event listener for app state changes. This change enhances the application's performance and user experience by providing efficient data fetching and synchronization with the server.

---
+++ src/query/QueryProvider.tsx
@@ -0,0 +1,26 @@
+import {
+  focusManager,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { PropsWithChildren, useEffect } from "react";
+import { AppState, AppStateStatus, Platform } from "react-native";
+
+function onAppStateChange(status: AppStateStatus) {
+  if (Platform.OS !== "web") {
+    focusManager.setFocused(status === "active");
+  }
+}
+export const _queryClient = new QueryClient();
+
+export const QueryProvider = ({ children }: PropsWithChildren) => {
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", onAppStateChange);
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+  return (
+    <QueryClientProvider client={_queryClient}>{children}</QueryClientProvider>
+  );
+};
